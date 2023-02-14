2b82130920b1f078d2ac7253936b308fce0df34d
sadra askari <122465265+sadraaskari@users.noreply.github.com>
2023-02-14 23:23:44 +0330

    further development of the database

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import CounselingRequest
+from .models import CounselingRequest, SliderImage, Message
 
 
 class CounselingRequestAdmin(admin.ModelAdmin):
@@ -9,4 +9,20 @@ class CounselingRequestAdmin(admin.ModelAdmin):
         return self.name
 
 
+class SliderImageAdmin(admin.ModelAdmin):
+    list_display = ('created_at', 'title', 'pk', 'image')
+
+    def __str__(self):
+        return self.title
+
+
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'pay_request', 'created_at', 'pk', 'sender_id', 'receiver_id')
+
+    def __str__(self):
+        return self.title
+
+
 admin.site.register(CounselingRequest, CounselingRequestAdmin)
+admin.site.register(SliderImage, SliderImageAdmin)
+admin.site.register(Message, MessageAdmin)
--- dashboard/models.py
+++ dashboard/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from users.models import UserProfile
 
 
 class CounselingRequest(models.Model):
@@ -11,3 +12,30 @@ class CounselingRequest(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class SliderImage(models.Model):
+    image = models.ImageField(upload_to='slider_images', blank=True)
+    created_at = models.DateTimeField(default=timezone.now)
+    title = models.CharField(max_length=30, default='')
+
+    def __str__(self):
+        return self.title
+
+
+class Message(models.Model):
+    title = models.CharField(max_length=30, default='')
+    pay_request = models.IntegerField(default=0)
+    created_at = models.DateTimeField(default=timezone.now)
+    description = models.TextField(default='')
+    sender = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    receiver = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name='receiver')
+
+    def sender_id(self):
+        return self.sender.user.id
+
+    def receiver_id(self):
+        return self.receiver.user.id
+
+    def __str__(self):
+        return self.title
