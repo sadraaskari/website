5106738f20b7c8b58b87818f79e76d406745fd5e
Mahbod <www.mahbodmodaresi@gmail.com>
2023-03-24 16:51:59 +0430

    new changes

---
+++ front-end/New folder/new codes/sideBar/dore/forMenu.js
@@ -0,0 +1,18 @@
+function showDiv(d){
+		alert(d);
+		for(var i = 1 ; i<= 4 ; i++){
+			var divBlocked = document.getElementById("d" + i);
+			if(divBlocked.style.display == "none"){
+				/*nothing*/
+			   }else{
+                   divBlocked.style.display = "none";
+               }
+		}
+		alert("fdkdfjdf");
+		var divOnClicked = document.getElementById("d" + d);
+		divOnClicked.style.display = "block";
+		
+	}
+    function test(){
+        alert("hello guys!!");
+    }
